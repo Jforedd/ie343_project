aa77fd540c4ae4dd91061ea0a20b2cdfa4e4fad1
Ezgi <168410873+Jforedd@users.noreply.github.com>
2024-05-17 15:02:59 +0300

    Update and rename README.md to Point.py

---
+++ Point.py
@@ -0,0 +1,9 @@
+
+class Point:
+    def __init__(self, id, x, y, demand):
+        self.id = id
+        self.x = x
+        self.y = y
+        self.demand = demand
+
+        
--- README.md
+++
@@ -1,1 +0,0 @@
-# ie343_project
